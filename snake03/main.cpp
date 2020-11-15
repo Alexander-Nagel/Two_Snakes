662b739897cfd92cecedf8dfb8460cfb51ab51af
Alex Nagel <mail@alex-nagel.de>
2020-11-15 01:00:20 +0100

    Initial Commit

---
+++ snake03/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  snake03
+//
+//  Created by Alexander Nagel on 15.11.20.
+//  Copyright © 2020 Alexander Nagel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
